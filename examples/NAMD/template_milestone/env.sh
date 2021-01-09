26d976c7d74f37328345939f4d69099a039bdf18
Dhiman Ray <dray1@uci.edu>
2021-01-09 13:27:10 -0800

    add slurm job submission script

--- examples/NAMD/template_milestone/env.sh
+++ examples/NAMD/template_milestone/env.sh
@@ -4,6 +4,6 @@
 export WEST_PYTHON=$(which python)
 # Actviate a conda environment containing westpa, openmm, mdtraj;
 # You may need to create this first (see install instructions)
-source activate westpa-openmm
+source activate westpa-openmm  #Use the name of the environment in which WESTPA is installed
 export WEST_SIM_ROOT="$PWD"
 export SIM_NAME=$(basename $WEST_SIM_ROOT)
